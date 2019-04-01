c62250806f5855b3335510c4a7c2b9cfa553a11c
MamkaForce <47280466+MamkaForce@users.noreply.github.com>
2019-04-01 17:21:22 +0300

    ready to check

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.getElementById("register-form").style.display = "none";
 var boxElement = document.getElementById("login-form");
 var boxElementTwo = document.getElementById("register-form")
 
@@ -8,19 +7,18 @@ function firstAnimation() {
         {opacity: 0}
         ], 1000);
         animation.addEventListener('finish', function() {
-        boxElement.style.opacity = 0;
-        });
-        if(boxElement.style.opacity == 0){
+          boxElement.style.opacity = 0;
           boxElement.style.display = "none";
-          boxElementTwo.style.display = ""; 
-        }
-        var animationTwo = boxElementTwo.animate([
-            {opacity: 0},
-            {opacity: 1}
-            ], 1000);
-            animationTwo.addEventListener('finish', function() {
-              boxElementTwo.style.opacity = 1;
-            })
+          boxElementTwo.style.display = "block";
+        });
+        
+    var animationTwo = boxElementTwo.animate([
+        {opacity: 0},
+        {opacity: 1}
+        ], 3000);
+        animationTwo.addEventListener('finish', function() {
+           boxElementTwo.style.opacity = 1;
+        })
 }
 
 function secondAnimation(){
@@ -30,16 +28,15 @@ function secondAnimation(){
       ], 1000);
       animationThree.addEventListener('finish', function() {
         boxElementTwo.style.opacity = 0;
-      });
-      if(boxElementTwo.style.opacity == 0){
         boxElementTwo.style.display = "none";
-        boxElement.style.display = "";
-        var anitionFour = boxElement.animate([
+        boxElement.style.display = "block";
+      });
+      
+      var anitionFour = boxElement.animate([
           {opacity: 0},
           {opacity: 1}
-        ], 1000)
+        ], 3000)
         anitionFour.addEventListener('finish', function() {
           boxElement.style.opacity = 1;
-        })
-      }
+        })      
 } 
